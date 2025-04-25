cec5c1e66f32207ba5189d60e6e3939354b55345
Mahamat335 <gcanbolat46@gmail.com>
2025-04-25 22:39:13 +0300

    add player camera

---
+++ Assets/Anonim/Scripts/Player/PlayerCamera.cs
@@ -0,0 +1,12 @@
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class PlayerCamera
+{
+    [SerializeField] private Camera _camera;
+    [SerializeField] private CinemachineCamera _cinemachineCamera;
+
+    private void Awake()
+    {
+    }
+}
